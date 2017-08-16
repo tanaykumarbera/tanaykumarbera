f432087b853049f860bd553060c6fe0e8d11ec5b
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-08-17 02:07:12 +0530

    Home Template 1. Recent blog posts 2. Twitter feed 3. Instagram feed 4. Contact via mail

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
+import Handlebars from 'handlebars-template-loader/runtime';
+
+/* IMPORT HANDLEBAR TEMPLATES */
 import renderLandingPage from './templates/index';
 import renderErrorPage from './templates/index';
+import post from './templates/post';
 
+/* IMPORT CORRESSPONDING PAGE STYLES */
 import styleLandingPage from './less/index';
 import styleErrorPage from './less/error';
 
+/* IMPORT json-ld SCHEMA */
 import schemaLandingPage from './schema.json';
 
+/* IMPORT SITE CONFIGURATION */
 import config from '../config';
 
+/* REGISTER POST TEMPLATE AS PATIAL */
+Handlebars.registerPartial('post', post);
+
+import posts from './tposts.js';
+
+/* EXPORT METHOD TO BE USED BY STATIC SITE GENERATOR */
 export default () => ({
   '/index.html': renderLandingPage({
     config,
-    styleLandingPage,
-    schema: JSON.stringify(schemaLandingPage)
+    style: styleLandingPage,
+    schema: JSON.stringify(schemaLandingPage),
+    posts
   }),
   '/error.html': renderErrorPage({
     config,
